196ec31ab0999472fe2ddee5a2dfbe57b5734185
Michele Brignola <michele.brignola2@gmail.com>
2026-03-23 11:37:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Carrier Service - Gruppo 1
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import './index.css'
+
+export default function App() {
+  return <h1>Ciao</h1>;
+}
