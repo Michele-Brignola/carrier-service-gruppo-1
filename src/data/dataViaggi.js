78e02a57d0a441017cf61d5d789e711c7674c8c4
Michele Brignola <michele.brignola2@gmail.com>
2026-03-23 15:52:01 +0100

    international Data name

--- src/data/dataViaggi.js
+++ src/data/dataViaggi.js
@@ -1,26 +1,26 @@
-export const dataViaggi = [
+export const dataTrip = [
   {
     id: 1,
-    nome: "Viaggio a Barcellona",
-    data_inizio: "2026-07-21",
-    data_fine: "2026-08-06",
+    name: "Viaggio a Barcellona",
+    start_date: "2026-07-21",
+    end_date: "2026-08-06",
   },
   {
     id: 2,
-    nome: "Viaggio in Giappone",
-    data_inizio: "2026-02-15",
-    data_fine: "2026-02-28",
+    name: "Viaggio in Giappone",
+    start_date: "2026-02-15",
+    end_date: "2026-02-28",
   },
   {
     id: 3,
-    nome: "Viaggio a Berlino",
-    data_inizio: "2026-09-08",
-    data_fine: "2026-09-19",
+    name: "Viaggio a Berlino",
+    start_date: "2026-09-08",
+    end_date: "2026-09-19",
   },
   {
     id: 4,
-    nome: "Viaggio a Mosca",
-    data_inizio: "2026-04-22",
-    data_fine: "2026-05-04",
+    name: "Viaggio a Mosca",
+    start_date: "2026-04-22",
+    end_date: "2026-05-04",
   },
 ];
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 export default function DefaultTemplate() {
   return (
     <>
-      <header className="bg-dark py-3 mb-4">
-        <div className="container d-flex align-items-center justify-content-between">
-          <h1 className="text-danger m-0">Boolflix</h1>
+      <header className="py-3">
+        <div className="container">
+          <h1 className="text-danger m-0">Trip Portal</h1>
         </div>
       </header>
       <Outlet />
