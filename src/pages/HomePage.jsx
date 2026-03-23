85487ff0372586ba46ded85589bf872f04f3ee35
MattiaGalasso <galasso.mattia94@gmail.com>
2026-03-23 16:46:46 +0100

    Add: trip list display with date normalization in HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,49 @@
+import { Link } from "react-router";
+import { dataTrip } from "../data/dataTrip";
+
 export default function HomePage() {
+  const dateNormalizer = (date) => {
+    const originalDate = new Date(date);
+    const normalizedData = originalDate.toLocaleDateString();
+    return normalizedData;
+  };
+
   return (
     <>
-      <h1>HOMEPAGE</h1>
+      <div className="container">
+        <h1 className="my-4">Trip List:</h1>
+        <div className="trip-list">
+          {dataTrip.map((trip) => (
+            <div className="card trip-list-card" key={trip.id}>
+              <div className="card-body">
+                <div className="card-trip-header">
+                  <h4 className="card-title">{trip.name}</h4>
+                  <span className="badge text-bg-success badge-trip-list">
+                    In Progress
+                  </span>
+                </div>
+                <div className="dates-cards">
+                  <div className="start-data-texts text-center">
+                    <span className="trip-title-card">Start Date: </span>
+                    <p className="trip-text-card">
+                      {dateNormalizer(trip.start_date)}
+                    </p>
+                  </div>
+                  <div className="end-data-texts text-center">
+                    <span className="trip-title-card">End Date: </span>
+                    <p className="trip-text-card">
+                      {dateNormalizer(trip.end_date)}
+                    </p>
+                  </div>
+                </div>
+                <Link to={`/trip/${trip.id}`} className="btn btn-primary">
+                  See More
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
