ccfd36f5e7f2aff358f84c130ae8a9e08f527b2d
Michele Brignola <michele.brignola2@gmail.com>
2026-03-23 14:14:22 +0100

    added testing data

---
+++ src/data/dataPartecipanti.js
@@ -0,0 +1,47 @@
+export const dataPartecipanti = [
+  {
+    id: 1,
+    nome: "Maurizio",
+    cognome: "Merluzzo",
+    email: "maurizio.merluzzo@gmail.com",
+    telefono: "+39 3348193021",
+    codice_fiscale: "MRLMRZ85M12H501Z",
+    id_viaggi: [1, 3]
+  },
+  {
+    id: 2,
+    nome: "Laura",
+    cognome: "Bianchi",
+    email: "laura.bianchi@gmail.com",
+    telefono: "+39 3471234567",
+    codice_fiscale: "BNCLRA92A41F205X",
+    id_viaggi: [2]
+  },
+  {
+    id: 3,
+    nome: "Marco",
+    cognome: "Ferretti",
+    email: "marco.ferretti@outlook.com",
+    telefono: "+39 3389876543",
+    codice_fiscale: "FRRMRC78T10L219K",
+    id_viaggi: [1]
+  },
+  {
+    id: 4,
+    nome: "Giulia",
+    cognome: "Romani",
+    email: "giulia.romani@yahoo.it",
+    telefono: "+39 3201122334",
+    codice_fiscale: "RMNGLI99P55G702W",
+    id_viaggi: [2, 3]
+  },
+  {
+    id: 5,
+    nome: "Stefano",
+    cognome: "Conti",
+    email: "stefano.conti@libero.it",
+    telefono: "+39 3664455667",
+    codice_fiscale: "CNTSFN88C03D612P",
+    id_viaggi: [4]
+  },
+];
