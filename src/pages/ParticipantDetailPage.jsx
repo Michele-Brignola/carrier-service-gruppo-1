9b17843012ec1a9552f8cca191b2180a3f6a6ecf
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-23 16:10:37 +0100

    detailpage

--- src/pages/ParticipantDetailPage.jsx
+++ src/pages/ParticipantDetailPage.jsx
@@ -1,7 +1,38 @@
+import { useParams } from 'react-router-dom';
+import { dataParticipant } from '../data/dataParticipant';
+
 export default function ParticipantDetailPage() {
+  const { id } = useParams();
+
+  const partecipante = dataParticipant.find((p) => p.id === parseInt(id));
+
+  if (!partecipante) {
+    return <h2>Partecipante non trovato</h2>;
+  }
+
   return (
-    <>
-      <h1>HOMEPAGE</h1>
-    </>
+    <div>
+      <h1>Dettaglio partecipante</h1>
+      <p>
+        <strong>Nome:</strong>
+        {partecipante.name}
+      </p>
+      <p>
+        <strong>Cognome:</strong>
+        {partecipante.surname}
+      </p>
+      <p>
+        <strong>Email:</strong>
+        {partecipante.email}
+      </p>
+      <p>
+        <strong>Telefono:</strong>
+        {partecipante.phone}
+      </p>
+      <p>
+        <strong>Codice Fiscale:</strong>
+        {partecipante.tax_id}
+      </p>
+    </div>
   );
 }
