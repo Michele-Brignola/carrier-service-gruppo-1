79ccb8a041093fa96d0a58a941b50f61e426fce3
Michele Brignola <michele.brignola2@gmail.com>
2026-03-23 16:43:03 +0100

    added img to trips

--- src/data/dataTrip.js
+++ src/data/dataTrip.js
@@ -1,25 +1,29 @@
 export const dataTrip = [
   {
     id: 1,
-    name: "Viaggio a Barcellona",
+    name: "Trip to Barcelona",
+    img: "barcelona.jpg",
     start_date: "2026-07-21",
     end_date: "2026-08-06",
   },
   {
     id: 2,
-    name: "Viaggio in Giappone",
+    name: "Trip to Tokyo",
+    img: "tokyo.jpg",
     start_date: "2026-02-15",
     end_date: "2026-02-28",
   },
   {
     id: 3,
-    name: "Viaggio a Berlino",
+    name: "Trip to Berlin",
+    img: "berlin.jpg",
     start_date: "2026-09-08",
     end_date: "2026-09-19",
   },
   {
     id: 4,
-    name: "Viaggio a Mosca",
+    name: "Trip to Moscow",
+    img: "moscow.jpg",
     start_date: "2026-04-22",
     end_date: "2026-05-04",
   },
